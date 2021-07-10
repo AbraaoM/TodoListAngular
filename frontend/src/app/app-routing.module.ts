8229d377ffbf0ce90c6ab7f502247cdb92de4be4
AbraaoM <abraaol.moreira@outlook.com>
2021-07-10 20:31:23 -0300

    :truck: set some routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { UpdateTaskComponent } from './components/task/update-task/update-task.component';
+import { CreateTaskComponent } from './components/task/create-task/create-task.component';
+import { CreateGroupComponent } from './components/group/create-group/create-group.component';
+import { UpdateGroupComponent } from './components/group/update-group/update-group.component';
 import { InfosComponent } from './views/infos/infos.component';
 import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
@@ -11,6 +15,22 @@ const routes: Routes = [
   {
     path:"infos",
     component: InfosComponent
+  },
+  {
+    path:"group/update",
+    component: UpdateGroupComponent
+  },
+  {
+    path:"group/create",
+    component: CreateGroupComponent
+  },
+  {
+    path:"task/create",
+    component: CreateTaskComponent
+  },
+  {
+    path:"task/update",
+    component: UpdateTaskComponent
   }
 ];
 
