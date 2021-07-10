424f3814da37347547dc0e79ced7c6d86babed52
AbraaoM <abraaol.moreira@outlook.com>
2021-07-10 16:26:33 -0300

    :truck: set initial routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { InfosComponent } from './views/infos/infos.component';
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent
+  },
+  {
+    path:"infos",
+    component: InfosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
