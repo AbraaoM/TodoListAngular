e9039f511104eba451cd9f9ea1158c7a314a37dd
AbraaoM <abraaol.moreira@outlook.com>
2021-07-11 19:23:49 -0300

    :art: add CRUD methods in task service

--- frontend/src/app/components/task/task.service.ts
+++ frontend/src/app/components/task/task.service.ts
@@ -1,5 +1,7 @@
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Task } from './task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +12,32 @@ export class TaskService {
   constructor(
     private http: HttpClient
   ) { }
+
+  create(task: Task): Observable<Task>{
+    return this.http.post<Task>(
+      this.baseUrl,
+      task
+    )
+  }
+
+  read(): Observable<Task[]>{
+    return this.http.get<Task[]>(
+      this.baseUrl
+    )
+  }
+
+  readById(id: number): Observable<Task>{
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Task>(url)
+  }
+
+  update(task: Task): Observable<Task> {
+    const url = `${this.baseUrl}/${task.id}`
+    return this.http.put<Task>(url, task)
+  }
+
+  delete(id: any): Observable<Task> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.delete<Task>(url)
+  }
 }
