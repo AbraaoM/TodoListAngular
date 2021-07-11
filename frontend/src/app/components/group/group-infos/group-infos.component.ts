a6b45cc1a845ed260167dc72419fdd85a4d550d2
AbraaoM <abraaol.moreira@outlook.com>
2021-07-11 19:18:17 -0300

    :art: show group title on info

--- frontend/src/app/components/group/group-infos/group-infos.component.ts
+++ frontend/src/app/components/group/group-infos/group-infos.component.ts
@@ -1,4 +1,7 @@
+import { Group } from './../group.model';
+import { GroupService } from './../group.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-group-infos',
@@ -6,16 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-infos.component.css']
 })
 export class GroupInfosComponent implements OnInit {
-  groupTitle: string = 'Nome do grupo'
+
+  group: Group = {
+    title: ''
+  }
+
   nTasks: number = 0
   nTasksMonth: number = 0
   nTasksMorning: number = 0
   nTasksEvening: number = 0
   nTasksNight: number = 0
 
-  constructor() { }
+  constructor(
+    private groupService: GroupService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id: any = this.route.snapshot.paramMap.get("id")
+    this.groupService.readById(id).subscribe((group) => {
+      this.group = group;
+    });
   }
 
 }
