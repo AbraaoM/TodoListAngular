d7f20d4c592a0b3780355a7a0396976f00abc53e
AbraaoM <abraaol.moreira@outlook.com>
2021-07-12 11:53:39 -0300

    :sparkles: set some task info items
    - days since task creation
    
    - task was created at morning, evening or night

--- frontend/src/app/components/task/task-infos/task-infos.component.ts
+++ frontend/src/app/components/task/task-infos/task-infos.component.ts
@@ -19,10 +19,8 @@ export class TaskInfosComponent implements OnInit {
     conclusionTime: ''
   }
 
-  taskTitle: string = 'Tarefa Genérica'
   daysFromCreation: number = 2
-  creationPeriod: string = 'tarde'
-  conclusionDate: string = '21/02/1994'
+  creationPeriod: string = ''
 
   constructor(
     private taskService: TaskService,
@@ -31,10 +29,32 @@ export class TaskInfosComponent implements OnInit {
     const id: any = this.route.snapshot.paramMap.get("id")
     this.taskService.readById(id).subscribe((task) => {
       this.task = task;
+
+      let time: Date = new Date(this.task.creationTime)
+      //Calcula quantidade de dias passados da criação da tarefa
+      const today: Date =  new Date()
+      console.log(time)
+
+      this.daysFromCreation = Math.ceil(
+                                Math.abs(
+                                  today.getTime() - time.getTime()
+                                ) / (1000 * 3600 * 24)
+                              )
+
+
+      //Calcula período de criação da tarefa (manhã, tarde ou noite)
+      if(time.getHours() >= 0 && time.getHours() < 12){
+        this.creationPeriod = 'Manhã'
+      }else if (time.getHours() >= 12 && time.getHours() < 18){
+        this.creationPeriod = 'Tarde'
+      }else{
+        this.creationPeriod = 'Noite'
+      }
     });
   }
 
   ngOnInit(): void {
+
   }
 
 }
